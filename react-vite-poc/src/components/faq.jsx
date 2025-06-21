9720d1b32af03339c762a71dd11d05a3f1fdbf76
Ted McCormick <ted.mccormick@gmail.com>
2025-06-21 00:10:56 -0500

    FAQing, I guess

--- react-vite-poc/src/components/faq.jsx
+++ react-vite-poc/src/components/faq.jsx
@@ -20,11 +20,10 @@ function FAQ() {
         </Accordion.Body>
       </Accordion.Item>
       <Accordion.Item eventKey="1">
-        <Accordion.Header className='bg-success-subtle text-white'>So everything is free?</Accordion.Header>
+        <Accordion.Header className='bg-success-subtle text-white'>So, seriously, everything is free?</Accordion.Header>
         <Accordion.Body>
           <div className="text-start">
-            <p>Yup, you got it chief.</p>
-            <p>There are enough free things that it's easier to talk about what's <em>not</em>free.</p>
+            <p>Yup, you heard right, chief. Most of Urmid is genuinely free. It's almost easier to tell you what's <em>not free:</em></p>
             <br />
             <p>If you've been chatting with someone, and you want to take it to the real world (which you should! the internet sucks, mkay?) you pay to ask them out.</p>
             <br />
@@ -40,8 +39,7 @@ function FAQ() {
         <Accordion.Body>
           <div className="text-start">
             <p>Woah, man, slow down. Take a deep breath.</p>
-            <p>That's fifty cents. Like, half a dollar.</p>
-            <p>Yup, you get someone to hang out with, a chance at true love and/or taking it to poundtown or whatever, and we get....enough money for like, an extra shot of syrup in our coffee.</p>
+            <p>That's fifty cents. As in, half a dollar. Not enough for a coffee, more like enough for an extra shot of syrup in ours. So yeah, for fifty cents, you get a chance to meet someone cool, chase true love (or whatever else you're into), and maybe even take it to Poundtown™. Pretty good deal, if you ask us.</p>
           </div>
         </Accordion.Body>
       </Accordion.Item>
@@ -50,7 +48,7 @@ function FAQ() {
         <Accordion.Body>
           <div className="text-start">
             <p>Nothing. We're really that stupid.</p>
-            <p>If you know an angel investor, put them in touch, because at some point these bags full of money we're lighting on fire are going to start running low, and shit's going to get wild. Quick.</p>
+            <p>If you know an angel investor, put them in touch, because at some point these bags full of money we're lighting on fire are going to start running low, and shit's going to get Lord the Flies pretty fucking quick.</p>
             
           </div>
         </Accordion.Body>
@@ -59,42 +57,78 @@ function FAQ() {
         <Accordion.Header>So once you get to a million users or whatever, you're going to start charging us $50 a month or something, right?</Accordion.Header>
         <Accordion.Body>
           <div className="text-start">
-            <p>Listen, if like, 30 people read this stupid line of text...well that's all we're (I'm) really out for.</p>
-            <p>Once we hit a couple of thousand users, things could get a little spicy - the guy that built this is an inveterate nerd who built it to be small, quick, and stupid cheap. But at about 10,000 users or so, it's going to start costing him money to run.</p>
-            <p>We'll probably go ad supported. Yeah, it sucks too, but...you'll ignore them, just like you ignore this FAQ, and keep chasing each other like horny, rabid dogs, and mostly it looks like the advertising will pay for the server costs.</p>
-            <p><strong>Mostly.</strong></p>
-            <p>But seriously. The plan is to stay free, forever. Avoid the dumb bullshit that's made every other app awful, and instead of being worth a couple of billion dollars (hi Match Group! Big fan of your investor relation reports, kthxbye!) our founder might get to quit his day job in a couple of years.</p>
+            <p>Honestly, if more than a handful of people read this FAQ, we've already exceeded expectations. But no, that's not the plan.</p>
+            <p>Our founder is a certified nerd who built Urmid to be lean, fast, and unbelievably cheap to run. While we're designed to scale, we know that hitting tens of thousands of users will eventually mean some real server costs.</p>
+            <p>When that happens, we'll likely become ad-supported. Yeah, ads can be annoying, but you're probably already ignoring them, just like you might be ignoring this very sentence. Our hope is that tasteful advertising will cover most of our operational expenses.</p>
+            <p><strong>Most.</strong></p>
+            <p>But seriously: the plan is to stay free, forever. Our goal isn't to become another multi-billion-dollar dating behemoth (hi Match Group! Big fan of your investor relation reports, kthxbye!). We just want to build an app that actually helps people find happiness, and maybe – just maybe – our founder can finally quit his day job someday.</p>
           </div>
         </Accordion.Body>
         </Accordion.Item>
-      <Accordion.Item eventKey="4">
-        <Accordion.Header>That....kind of sounds like everything?</Accordion.Header>
+      <Accordion.Item eventKey="5">
+        <Accordion.Header>So there's really no catch? Are you, like, selling our DNA to the Russian mob? Is this a crypto scam?</Accordion.Header>
         <Accordion.Body>
-          Yeah, we weren't kidding. Most things are free!<br />
-            <br />
-          <strong>BUT</strong> there are a few things. Free chat only lasts five days, and then you have to stop wasting our server capacity (oh and also your match's time) - shit or get off the pot brah.<br />
-          <br />
-          If you propose a date, and the other person accepts, there's a small cost. We're toying with $.50, although that's subject to change.
+          <div className="text-start">
+            <p>No but the Russian mob thing sounds interesting - put their people in touch with our people, would you?</p>
+            <p>Urmid is built on a promise: Free forever. You pay a tiny fee for Date Requests when you find someone awesome. You pay a small 'Success Fee' when you leave us because you found somebody even more awesome. That's the deal. No fine print.</p>
+            <p>We don't sell your data, we don't sell your DNA, and honestly, if we didn't legally have to keep an email address on file, we probably wouldn't even ask for that.</p>
+          </div>
         </Accordion.Body>
       </Accordion.Item>
             <Accordion.Item eventKey="6">
-        <Accordion.Header>FIFTY DOLLARS FOR A DATE?!?!? THAT'S OUTRAGEOUS!!!!</Accordion.Header>
+        <Accordion.Header>What if I don't want to pay 50 cents for a Date Request? Are you going to stop me from exchanging info in chat?</Accordion.Header>
         <Accordion.Body>
-          There's a decimal there. Fifty <em>cents.</em> As in half of a dollar.<br />
-            <br />
-          <strong>BUT</strong> there are a few things. Free chat only lasts five days, and then you have to stop wasting our server capacity (oh and also your match's time) - shit or get off the pot brah.<br />
-          <br />
-          If you propose a date, and the other person accepts, there's a small cost. We're toying with $.50, although that's subject to change.
+          <div className="text-start">
+            <p>We're not.</p>
+            <p>Seriously, if 50 cents is going to break your dating budget, email us at ceo@urmid.com. We'll spot you a lifetime supply of internet cool points.</p>
+            <p>Now, to keep things safe and not totally chaotic, we do have some clever monitoring in our Vibe Chats. It flags obvious (and not-so-obvious) attempts to exchange contact info like emails, phone numbers, or external websites. Not because we don't want you to connect – we absolutely do! But because spammers, trolls, and scammers use those exact same tactics. It's about protecting you, not preventing your love life.</p>
+            <p>So, if you want to encode your Signal username by arranging pages from War and Peace into hexadecimal values within the chat... hey man, go off. Just know you'd be impressing each other by essentially swindling your future favorite tech billionaire out of 50 cents worth of server fuel. Your call.</p>
+          </div>
         </Accordion.Body>
       </Accordion.Item>
       <Accordion.Item eventKey="7">
-        <Accordion.Header>Oh. That's...not much.</Accordion.Header>
+        <Accordion.Header>Wait. You're okay with that?</Accordion.Header>
         <Accordion.Body>
-          There's a decimal there. Fifty <em>cents.</em> As in half of a dollar.<br />
-            <br />
-          <strong>BUT</strong> there are a few things. Free chat only lasts five days, and then you have to stop wasting our server capacity (oh and also your match's time) - shit or get off the pot brah.<br />
-          <br />
-          If you propose a date, and the other person accepts, there's a small cost. We're toying with $.50, although that's subject to change.
+          <div className="text-start">
+            <p>Look, we're a sticklers for rules, especially since we're the one who wrote 'em. But I'm not your mom, or your parole officer. (Though I'm almost as fat as your mom, and definitely way sluttier than your parole officer.)</p>
+            <p>It's the same deal with closing your account. You can tell us you're closing it for any reason – you're tired of our shenanigans (fair!), you've embraced a life of celibacy, you're moving to Mars. You can even tell us Urmid sucks, go get married, and yup, keep your five bucks. Spend it on your honeymoon, or on hookers, or on hookers on your honeymoon. We won't judge.</p>
+            <p>We're more likely to start publicly begging for donations (shoutout to Wikipedia! We love... everything about you, actually) than to ever try to screw you over. We're about genuine connections, not nickel-and-diming you.</p>
+            <p>Free. Forever. That's the promise.</p>
+          </div>
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="8">
+        <Accordion.Header>How do you keep answering questions and I feel like I have more questions than before? HOW? WHY? WHAT, I guess?</Accordion.Header>
+        <Accordion.Body>
+          <div className="text-start">
+            <p>The 'how' is simple: our founder is an inveterate nerd who basically built Urmid on a whim. He stumbled upon some Reddit rants about how agonizing it is to find someone decent these days, and thought, "I can do better." He's a big guy (in both nerdiness and physical stature) who specializes in building incredibly snazzy, performant, and absurdly cheap cloud infrastructure. Like, borderline free.</p>
+            <p>The 'why' is a bit more philosophical. The world needs more happiness, more genuine human connection, and yes, even more hooking up. Life's often a dumpster fire, but if Urmid can spark a little joy in people's lives and help them connect... that'd be pretty damn cool.</p>
+            <p>And for those angel investors out there (you might want to cover your ears for this part): screw capitalism. Why should finding someone to hang out with, or even to get laid, suddenly come with a hefty price tag? It's ridiculous.</p>
+            <p>So, yeah. This is genuinely an 'audacity of hope' kind of venture. Which, if you think about it, is a pretty accurate description of dating itself.</p>
+          </div>
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="9">
+        <Accordion.Header>Okay, but nerdy is my love language. How?</Accordion.Header>
+        <Accordion.Body>
+          <div className="text-start">
+            <p>React front end, statically hosted by a CDN.</p>
+            <p>Go backend, running on function-as-a-service providers who I will name drop in a fucking millisecond for some server credits (call me!), all routed through a variety of Restful and Websocket connections.</p>
+            <p>Database architecture running on free/cheap managed services. Props there - if there's somebody else offering SQL on a free tier, I sure couldn't find them, but Oracle has a nice offering (and I will sing your praises more for some server credits, boys!). There are a couple of databases, though - the other is a serverless document database with a nice free tier offering.</p>
+            <p>If that's interesting, or sensical to you, and you'd like to join us....well, I wouldn't hold my breath, I can't even hire <em>my own damn self</em> yet. But what the hey: ceo@urmid.com; send your resume and....whatever else it is that people do to get noticed in tech nowadays. Nudes? Some sort of AI api key? Sure. Fire away.</p>
+          </div>
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="10">
+        <Accordion.Header>Okay, but nerdy is my love language. How?</Accordion.Header>
+        <Accordion.Body>
+          <div className="text-start">
+            <p>React front end, statically hosted by a CDN.</p>
+            <p>Go backend, running on function-as-a-service providers who I will name drop in a fucking millisecond for some server credits (call me!), all routed through a variety of Restful and Websocket connections.</p>
+            <p>Database architecture running on free/cheap managed services. Props there - if there's somebody else offering SQL on a free tier, I sure couldn't find them, but Oracle has a nice offering (and I will sing your praises more for some server credits, boys!). There are a couple of databases, though - the other is a serverless document database with a nice free tier offering.</p>
+            <p>If that's interesting, or sensical to you, and you'd like to join us....well, I wouldn't hold my breath, remember the part about not being able to hire <em>my damn self</em> yet?</p>
+            <p>But whatever. ceo@urmid.com; send your resume and....whatever else it is that people do to get noticed in tech nowadays. Nudes? Some sort of AI api key? Sure. Fire away.</p>
+          </div>
         </Accordion.Body>
       </Accordion.Item>
     </Accordion>
