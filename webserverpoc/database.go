4f459f88236daca80dbe30567c4af8808dc769a0
Ted McCormick <ted.mccormick@gmail.com>
2025-06-23 19:50:42 -0500

    Still databses. Or databas-i. You get it

--- webserverpoc/database.go
+++ webserverpoc/database.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
@@ -12,17 +14,17 @@ func ConnectToMongoDB() (*mongo.Client, error) {
 	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017") // Or your Atlas connection string
 	client, err := mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
-		// Handle error
+		fmt.Errorf("Failed to connect to MongoDB: " + err.Error())
 	}
 	return client, nil
 }
 
 func ConnectToMySQL() (*gorm.DB, error) {
-	dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(gorm.Open(dsn), &gorm.Config{})
+	dsn := "user:password@tcp(localhost:3306)/database?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
-		// Handle error
+		fmt.Errorf("Failed to connect to MySQL: " + err.Error())
 	}
 
 	return db, nil
